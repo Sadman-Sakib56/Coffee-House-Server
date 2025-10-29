d251900f4403630a60081534e5803a19308f070d
Sadman Sakib <sadmansakib60356@gmail.com>
2025-10-30 02:08:48 +0600

    post-functionality-added

--- index.js
+++ index.js
@@ -34,6 +34,22 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const coffeeCollection = client.db('coffee-house-admin').collection('coffees')
+
+        // post from add coffee
+        app.post('/coffees', async (req, res) => {
+            const newCoffee = req.body;
+            console.log((newCoffee));
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
